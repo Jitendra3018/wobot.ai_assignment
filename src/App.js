a302ce589c1040dd5be4db9cdfccac31d6312277
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2023-01-12 12:28:21 +0530

    Logo Section Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import logo from "./assets/wobot-logo_blue.png";
 function App() {
     return (
         <div className="container">
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+            </div>
+
         </div>
     );
 }
